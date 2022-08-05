b9a24f51e380f2bf2877b631ff7fb21c912525d2
Moritz Huebner <moritz.huebner@tngtech.com>
2022-08-05 17:54:29 +1000

    fixin' things
    Signed-off-by: Moritz Huebner <moritz.huebner@tngtech.com>

--- lte/gateway/python/setup.py
+++ lte/gateway/python/setup.py
@@ -111,7 +111,7 @@ setup(
         'wsgiserver>=1.3',
         # pin recursive dependencies of ryu and others
         'charset-normalizer==2.0.0',
-        'docker==5.0.3',
+        'docker==4.1.0',
         'urllib3>=1.25.3',
         'websocket-client>=1.3.2',
         'requests==2.28.1',
