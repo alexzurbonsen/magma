44362ba599d0aacbefa551ecc7388ee5d4ca5298
Moritz Huebner <moritz.huebner@tngtech.com>
2022-08-08 11:23:21 +1000

    increment urllib3
    Signed-off-by: Moritz Huebner <moritz.huebner@tngtech.com>

--- lte/gateway/python/setup.py
+++ lte/gateway/python/setup.py
@@ -112,7 +112,7 @@ setup(
         # pin recursive dependencies of ryu and others
         'charset-normalizer==2.0.0',
         'docker==4.1.0',
-        'urllib3>=1.25.3',
+        'urllib3>=1.26.11',
         'websocket-client>=1.3.2',
         'requests==2.28.1',
         'certifi>=2022.6.15',
